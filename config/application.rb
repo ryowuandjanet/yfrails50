505287fe4ef97f5f8cfd3417e4fc63faec4a8d8d
ryowuandjanet <ryowudata@gmail.com>
2020-04-05 00:04:19 +0800

    在application.rb裡加入config.assets.compile = true

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Yangfurails6
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.compile = true
     config.assets.enabled= true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
